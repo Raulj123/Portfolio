f90bfe736d204ddf70224bec07fbbb61550aaebe
raul <jarquinr121@gmail.com>
2023-04-24 15:19:06 -0700

    projects mobile done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -156,6 +156,7 @@
 {/if}
 <hr />
 
+{#if !mobile}
 <section id="projects">
 	<h1 class="text-center p-20" style=" font-weight: bold;">Projects</h1>
 	<div class="cardContainer">
@@ -188,7 +189,48 @@
 	</div>
 	<!-- <a href="https://github.com/Raulj123"style="text-decoration: none; color: #cdf0f6; " >See more on my GitHub</a> -->
 </section>
+{:else}
+<section id="projects">
+	<h1 class="text-center p-20" style=" font-weight: bold;">Projects</h1>
+	<div class="cardContainer">
+		{#each projectDetail as projects}
+			<div class="cardMobile">
+				<a href={projects.href} style="text-decoration: none; color: #cdf0f6;" target="_blank">
+					<img
+						class="text-center centered-img h-35 w-75"
+						style="border-radius-bottom:9px;"
+						src={projects.image}
+						alt="Notess web screenshot"
+					/>
+				</a>
+				<h3 class="text-left p-2" style=" font-weight: bold; font-size:1.1rem;">{projects.title}</h3>
+				{#each projects.badges as badge, i}
+					<span class="badge variant-filled ml-3" style="font-size:.5rem;"
+						>{badge}
+						<img
+							class="text-center u-max-full-width centered-img h-4 w-4 p-1"
+							style="background-color: transparent;"
+							src={projects.badgesPic[i]}
+							alt={badge}
+						/>
+					</span>
+				{/each}
 
+				<p class="text-center py-3" style="font-size:.8rem;">{projects.content}</p>
+			</div>
+		{/each}
+	</div>
+	<!-- <a href="https://github.com/Raulj123"style="text-decoration: none; color: #cdf0f6; " >See more on my GitHub</a> -->
+</section>
+
+
+
+
+
+
+
+
+{/if}
 <hr />
 
 <section id="experience">
@@ -254,6 +296,18 @@
 		padding-top: 0px;
 		margin-right: 20px;
 		margin-left: 20px;
+		margin-bottom: 25px;
+		border-radius: 20px;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+		transition: box-shadow 0.3s ease-in-out;
+		transition: transform 0.3s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+	}
+	.cardMobile{
+		max-width: 250px;
+		padding-top: 0px;
+		margin-right: 20px;
+		margin-left: 20px;
+		margin-bottom: 25px;
 		border-radius: 20px;
 		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
 		transition: box-shadow 0.3s ease-in-out;

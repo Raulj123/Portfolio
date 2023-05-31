2c099d7df3e753a0fb29a4627d9ac96460d36f22
IamRaul <jarquinr121@gmail.com>
2023-05-30 19:09:23 -0700

    ♻️

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { AppBar, Toast, toastStore, Accordion, AccordionItem,LightSwitch, clipboard } from '@skeletonlabs/skeleton';
 	import type { experience, navLink, projects } from '../app';
   import type {ToastSettings} from '@skeletonlabs/skeleton'
+  import {projects} from './projects.ts'
 	let innerWidth = 0
   function pokeMe(){
     const t: ToastSettings ={
@@ -47,46 +48,6 @@
 		{
 			title: 'Contact',
 			href: 'contact',
-		}
-	];
-
-	let projectDetail: projects[] = [
-		{
-			href: 'https://github.com/Raulj123/FeelingsHub',
-			image: './feelingsHub.png',
-			title: 'FeelingsHub',
-			badges: ['Svelte', 'TypeScript', 'ElephantSQL','Prisma'],
-			badgesPic: ['./svelte.png', './ts.svg', './postgresql.png','./prisma.png'],
-			content:
-				'Built and designed FeelingsHub, a web application using Svelte, Prisma, and ElephantSQL, where users can share their emotions. Created a user-friendly interface with full CRUD functionality for easy input, viewing, editing, and deletion of feelings.'
-		},
-		{
-			href: 'https://github.com/Raulj123/Notess',
-			image: './Notess.png',
-			title: 'Notess',
-			badges: ['PHP', 'MySQL'],
-			badgesPic: ['./php.svg', 'MySql.svg'],
-			content:
-				'Notess is a web-based note-taking application that allows users to create accounts, log in securely, and save notes. The app is designed to provide a user-friendly platform for taking and storing notes. Users can retrieve and edit their notes at any time'
-		},
-
-		{
-			href: 'https://github.com/Raulj123/CodeSnippet',
-			image: './CodeShare.png',
-			title: 'CodeShare',
-			badges: ['Svelte', 'TypeScript'],
-			badgesPic: ['./svelte.png', './ts.svg'],
-			content:
-				'Code share is a web-based code snippet manager that simplifies the process of storing and organizing code snippets. Users can add a title, programming language, and code snippet, which are displayed in a easy-to-read format using the Skeleton UI framework. The application allows users to delete unwanted code snippets or mark important ones as favorites.'
-		},
-		{
-			href: 'https://github.com/Raulj123/c-vis',
-			image: './vis.png',
-			title: 'Sorting Visualizer',
-			badges: ['C++', 'SDL'],
-			badgesPic: ['./c.svg', 'SDL.svg'],
-			content:
-				'Program that implements a visualization of various sorting algorithms. The user is prompted to select a sorting algorithm and the program generates a vector of 100 random integers. The chosen sorting algorithm is then applied to the vector, and at each step of the algorithm, the program visually displays the current state of the vector as a series of vertical lines on a window.'
 		}
 	];
 
@@ -187,7 +148,7 @@
 <section id="projects">
 	<h1 class="text-center p-20" style=" font-weight: bold;">Projects</h1>
 	<div class="cardContainer">
-		{#each projectDetail as projects}
+		{#each projects as projects}
 			<div class="card">
 				<a href={projects.href} style="text-decoration: none; color: #cdf0f6;" target="_blank">
 					<img
@@ -220,7 +181,7 @@
 <section id="projects">
 	<h1 class="text-center p-20" style=" font-weight: bold;">Projects</h1>
 	<div class="cardContainer">
-		{#each projectDetail as projects}
+		{#each projects as projects}
 			<div class="cardMobile">
 				<a href={projects.href} style="text-decoration: none; color: #cdf0f6;" target="_blank">
 					<img
---
+++ src/routes/projects.ts
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+			href: 'https://github.com/Raulj123/FeelingsHub',
+			image: './feelingsHub.png',
+			title: 'FeelingsHub',
+			badges: ['Svelte', 'TypeScript', 'ElephantSQL','Prisma'],
+			badgesPic: ['./svelte.png', './ts.svg', './postgresql.png','./prisma.png'],
+			content:
+				'Built and designed FeelingsHub, a web application using Svelte, Prisma, and ElephantSQL, where users can share their emotions. Created a user-friendly interface with full CRUD functionality for easy input, viewing, editing, and deletion of feelings.'
+		},
+		{
+			href: 'https://github.com/Raulj123/Notess',
+			image: './Notess.png',
+			title: 'Notess',
+			badges: ['PHP', 'MySQL'],
+			badgesPic: ['./php.svg', 'MySql.svg'],
+			content:
+				'Notess is a web-based note-taking application that allows users to create accounts, log in securely, and save notes. The app is designed to provide a user-friendly platform for taking and storing notes. Users can retrieve and edit their notes at any time'
+		},
+		{
+			href: 'https://github.com/Raulj123/CodeSnippet',
+			image: './CodeShare.png',
+			title: 'CodeShare',
+			badges: ['Svelte', 'TypeScript'],
+			badgesPic: ['./svelte.png', './ts.svg'],
+			content:
+				'Code share is a web-based code snippet manager that simplifies the process of storing and organizing code snippets. Users can add a title, programming language, and code snippet, which are displayed in a easy-to-read format using the Skeleton UI framework. The application allows users to delete unwanted code snippets or mark important ones as favorites.'
+		},
+		{
+			href: 'https://github.com/Raulj123/c-vis',
+			image: './vis.png',
+			title: 'Sorting Visualizer',
+			badges: ['C++', 'SDL'],
+			badgesPic: ['./c.svg', 'SDL.svg'],
+			content:
+				'Program that implements a visualization of various sorting algorithms. The user is prompted to select a sorting algorithm and the program generates a vector of 100 random integers. The chosen sorting algorithm is then applied to the vector, and at each step of the algorithm, the program visually displays the current state of the vector as a series of vertical lines on a window.'
+		}
+]
